ac547cf97d30e36f77426311dc03582dea6ebaa9
unknown <santhoshyavd@gmail.com>
2022-07-13 22:29:25 +0530

    states

--- src/styles/components/Board.jsx
+++ src/styles/components/Board.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import React , {useState} from 'react';
 import Square from './Square'
 
 const Board = () => {
+  const [board,setBoard]=useState(Array(9).fill(null));
+  const [isxnext,setx]=useState(false);
+  const hsc = (pos)=>{
+    if(board[pos]){
+      return;
+    }
+    setBoard(prev=> {
+      return prev.map((square,p)=>{
+        if(p===pos){
+          return isxnext?'X':'O';
+        }
+        return square;
+      });
+    });
+  setx((prev)=>!prev);
+};
+const rs=(pos)=>{
+  return <Square value={board[pos]} onClick={()=>{
+    hsc(pos)
+}}/>
+}
   return (
     <div className='board'>
         <div className='board-row'>
-            <Square value={1} />
-            <Square value={2}/>
-            <Square value={3}/>
+            {rs(0)}
+            {rs(1)}
+            {rs(2)}
         </div>
         <div className='board-row'>
-            <Square value={4}/> 
-            <Square value={5}/> 
-            <Square value={6}/>
+          {rs(3)}
+          {rs(4)}
+          {rs(5)}
         </div>
         <div className='board-row'>
-            <Square value={7}/>
-            <Square value={8}/>
-            <Square value={9}/>
-        </div>
+            {rs(6)}
+            {rs(7)}
+            {rs(8)}
+          </div>
     </div>
   );
 }
