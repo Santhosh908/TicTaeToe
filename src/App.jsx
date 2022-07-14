3679bb8016d4a0f6b6957d9fd138f3e833585ce9
unknown <santhoshyavd@gmail.com>
2022-07-14 22:00:19 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,51 @@
-import React,{useState} from 'react';
-import { calculateWinner } from './helpers';
+import React, { useState } from 'react';
 import Board from './styles/components/Board';
+import { calculateWinner } from './helpers';
+
 import './styles/root.scss';
 
-
 const App = () => {
-  const [history,sethistory]=useState([{ board:Array(9).fill(null),isxnext: true},]);
-  const [currentMove,setcurrentMove] = useState(0);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
 
   const current = history[currentMove];
   
-  // const [isxnext,setx]=useState(false);
-  const winner=calculateWinner(current.board);
-  const message=winner?`Winner is ${winner}`:`Next player is ${(current.isxnext) ?'X' :'O'}`;
-  const hsc = pos=>{
-    if(current.board[pos] ||winner){
+  console.log('history', history);
+
+  const winner = calculateWinner(current.board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
+  const hsc = position => {
+    if (current.board[position] || winner) {
       return;
     }
-    
-    sethistory(prev=> {
-      const last= prev[prev.length-1];
 
-      const newBoard=last.map((square,p)=>{
-        if(p===pos){
-          return last.isxnext?'X':'O';
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
         }
+
         return square;
       });
-      return prev.concat({board: newBoard, isxnext: !last.isxnext});
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
-  setcurrentMove(prev=>prev+1);
-};
+
+    setCurrentMove(prev => prev + 1);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <p>Have fun:)</p>
       <h2>{message}</h2>
-      <Board board={current.board} hsc={hsc}/>
+      <Board board={current.board} hsc={hsc} />
     </div>
   );
 };
